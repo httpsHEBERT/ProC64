8220c5374b9ce56ae3f1418bf2ed97e4821b4519
Hebert Luís Gomes Gonçalves <httpshebert@gmail.com>
2022-11-26 08:05:47 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,64 @@
+const dictionary = {
+
+  "o": {
+    "word": "O",
+    "lexicalCategory": "Artigo definido",
+    "definition": "   É o tipo de artigo usado para determinar de forma precisa os substantivos que o seguem. Nesse caso, está no singular masculino."
+  },
+
+  "india": {
+    "word": "India",
+    "lexicalCategory": "Substantivo próprio",
+    "definition": "   Um país no sul da Ásia ocupando a maior parte do subcontinente indiano; população 1.311.000.000 (estimada em 2015); línguas oficiais, Hindi e Inglês (quatorze outras línguas são reconhecidas como oficiais em certas regiões; destas, Bengali, Gujarati, Marathi, Tamil, Telugu e Urdu têm a maioria dos falantes como primeira língua); capital, Nova Delhi."
+  },
+
+  "externamente": {
+    "word": "Externamente",
+    "lexicalCategory": "Advérbio de lugar",
+    "definition": "   Com referência à superfície externa ou estrutura de algo; lado de fora."
+  },
+
+  "olá": {
+    "word": "Olá",
+    "lexicalCategory": "Interjeição",
+    "definition": "   Usado como uma saudação ou para iniciar uma conversa telefônica."
+  },
+
+  "bem-vindo": {
+    "word": "Bem-vindo",
+    "lexicalCategory": "Substantivo",
+    "definition": "   Uma saudação ou maneira de cumprimentar alguém."
+  },
+
+  "quase": {
+    "word": "Quase",
+    "lexicalCategory": "Advérbio de modo/intensidade",
+    "definition": "   Não exatamente; muito próximo."
+  },
+
+  "ela": {
+    "word": "Ela",
+    "lexicalCategory":"Pronome pessoal do caso reto",
+    "definition" : "   Usado para se referir a uma mulher, menina ou animal fêmea previamente mencionado ou facilmente identificado."
+  },
+
+  "árvore": {
+    "word": "Árvore",
+    "lexicalCategory": "Substantivo simples",
+    "definition": "   Uma planta perene lenhosa, geralmente tendo um único caule ou tronco crescendo a uma altura considerável e tendo ramos laterais a uma certa distância do solo."
+  },
+
+  "maravilhoso": {
+    "word": "Maravilhoso",
+    "lexicalCategory": "Adjetivo",
+    "definition": "   Alegria inspiradora, prazer ou admiração; extremamente bom; maravilhoso."
+  },
+
+  "comum": {
+    "word": "Comum",
+    "lexicalCategory": "Adjetivo",
+    "definition" : "   Sem características especiais ou distintivas; normal."
+  }
+}
+
+export default dictionary
